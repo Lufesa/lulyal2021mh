469f34db1f182b27c1a5aab29571bab2114c5010
Also99 <abouchedid.aldo@gmail.com>
2021-01-30 22:02:31 -0500

    Added images

---
+++ src/ObjectHolder.java
@@ -0,0 +1,21 @@
+
+public class ObjectHolder {
+	
+	//Attributes
+	
+	private int height;
+	private int width;
+	private Body body;
+	
+	//Constructor
+	public ObjectHolder(int height, int width, Body body) {
+		this.height = height;
+		this.width = width;
+		this.body = body;
+	}
+	
+	public void fitToSize(Body body) {
+		
+	}
+
+}
